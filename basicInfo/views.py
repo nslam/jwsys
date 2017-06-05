39e7bfbbb25f8b67431fb4f06e00d4fff3857fc6
TheLam <nslam@zju.edu.cn>
2017-06-05 12:01:13 +0800

    add some views

--- basicInfo/views.py
+++ basicInfo/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from .models import *
+from django.contrib.auth import authenticate
+from django.http import HttpResponse, JsonResponse
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+def login(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    if username is None:
+        return HttpResponse('username cannot be none!')
+    if password is None:
+        return HttpResponse('Password cannot be none!')
+    user = authenticate(username=username, password=password)
+    if user is None:
+        return HttpResponse('Password is not correct!')
+    return HttpResponse('Login successed!')
+
+
+@login_required
+def setPassword(request):
+    user = authenticate(username=request.user.username, password=request.POST['oldPassword'])
+    if user is None:
+        return HttpResponse('Password is wrong!')
+    user.set_password(request.POST['newPassword'])
+    return HttpResponse('Password changed!')
+
+
--- jwsys/settings.py
+++ jwsys/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 's$afl%1xr-q^fb(=xukd5*sth^$)x(1z2oy)gjogggbwv=-a2!'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -70,21 +68,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'jwsys.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME':'jwsysDB',
+        'NAME': 'jwsysDB',
         'USER': 'root',
         'PASSWORD': 'thelam',
         'HOST': 'localhost',
         'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -104,7 +100,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
@@ -118,8 +113,8 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+LOGIN_URL = '/login/'
