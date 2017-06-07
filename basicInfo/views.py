23d2bed538f1e82bd235a0af39ba9c9d0fea6008
keys961 <keys961@hotmail.com>
2017-06-07 23:25:20 +0800

    Update HTML files

--- basicInfo/views.py
+++ basicInfo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from .models import *
 from django.contrib.auth import authenticate
 from django.http import HttpResponse, JsonResponse
@@ -6,49 +6,78 @@ from django.contrib.auth.decorators import login_required
 
 
 def index(request):
-    return HttpResponse('Hello,you are at the basicInfo page.')
+    status = 'true'
+    return render(request, 'login.html', {'status': status})
 
 
 def login(request):
     username = request.POST['username']
     password = request.POST['password']
+    type = request.POST['type']
+    status = 'true'
     # logs = Log.objects.filter(event__contains=request.POST['username'])
     # if login too frequent:
     #     return HttpResponse('Log in too frequent!')
     # pass
-    if username is None:
-        return HttpResponse('username cannot be none!')
-    if password is None:
-        return HttpResponse('Password cannot be none!')
+    if username is None or password is None:
+        status = 'User name or password cannot be none!'
+        return render(request, 'login.html', {'status': status})
     user = authenticate(username=username, password=password)
     if user is None:
-        return HttpResponse('Password is not correct!')
-    return HttpResponse('Login successed!')
+        status = 'Password is not correct!'
+        return render(request, 'login.html', {'status': status})
+    # Need to test the type of the user according to DB
+    # Session or Cookies may be needed, request.user needs to change globally
+    # m = Member.objects.get(username=username)
+    # request.session['member_id'] = m.id
+    if type == 'Student':
+        return render(request, 'student_main.html', {'username': username})
+    elif type == 'Teacher':
+        return render(request, 'instructor_main.html', {'username': username })
+    elif type == 'Manager':
+        return render(request, 'manager_main.html', {'username': username})
 
 
 @login_required
 def setPassword(request):
-    flag = request.user.check_password(request.POST['oldPassword'])
-    if flag is False:
-        return HttpResponse('Password is wrong!')
-    request.user.set_password(request.POST['newPassword'])
-    return HttpResponse('Password changed!')
+    if request.method == 'GET':
+        return render(request, 'changepwd.html', {'status':'true'})
+    else:
+
+        originPwd = request.POST['originPwd']
+        newPwd = request.POST['newPwd']
+        newPwdAgain = request.POST['newPwdAgain']
+        #Session may be needed
+        usr = request.user
+        flag = request.user.check_password(originPwd)
+        if flag is False:
+            status = 'Original password is not correct!'
+        elif newPwd != newPwdAgain:
+            status = 'New passwords are not same!'
+        else:
+            request.user.set_password(request.POST['newPwd'])
+            status = 'Password changed successful!'
+        return render(request, 'changepwd.html', {'status':status})
 
 
 @login_required
 def changeInfo(request):
-    if request.POST['identity'] == 'student':
-        info = request.user.student
-    elif request.POST['identity'] == 'instructor':
-        info = request.user.instructor
-    elif request.POST['identity'] == 'manager':
-        info = request.user.manager
-    if info is None:
-        return HttpResponse('No such person!')
-    info.address = request.POST['address']
-    info.phone_number = request.POST['phone_number']
-    info.save()
-    return HttpResponse('success')
+    if request.method == 'GET':
+        #Fetch info into the form & render
+        return render(request, 'personal_info.html')
+    else:
+        if request.POST['identity'] == 'student':
+            info = request.user.student
+        elif request.POST['identity'] == 'instructor':
+            info = request.user.instructor
+        elif request.POST['identity'] == 'manager':
+            info = request.user.manager
+        if info is None:
+            return HttpResponse('No such person!')
+        info.address = request.POST['address']
+        info.phone_number = request.POST['phone_number']
+        info.save()
+        return HttpResponse('success')
 
 
 @login_required
@@ -135,58 +164,58 @@ def searchLog(request):
     return JsonResponse(ret)
 
 
-@login_required
-def inputScore(request):
-    if request.user.instructor is None:
-        return HttpResponse('You are not a instrctor!')
-    section = section.Objects.get(id=request.POST['sectionId'])
-    students = section.student_set.all()
-    for student in students:
-        insert
-        record
-        to
-        Takes
-    return HttpResponse('success')
-
-
-@login_required
-def teacherGetScore(request):
-    if request.user.instructor is None:
-        return HttpResponse('You are not a instrctor!')
-    section = section.Objects.get(id=request.POST['sectionId'])
-    students = section.student_set.all()
-    ret = []
-    for student in students:
-        ret.append({'id': student.id, 'score': student.score})
-    return JsonResponse(ret)
-
-
-@login_required
-def changeScore(request):
-    if request.user.instructor is None:
-        return HttpResponse('You are not a instrctor!')
-    if request.user.check_password(request.POST['password']) is False:
-        return HttpResponse('Password is wrong!')
-    section = section.Objects.get(id=request.POST['sectionId'])
-    student = request.POST['studentId']
-    student.score = request.POST['score']
-    return HttpResponse('success')
-
-
-@login_required
-def studentGetScore(request):
-    if request.user.student is None:
-        return HttpResponse('You are not a student!')
-    student = request.user.student
-    sections = student.section_set.all()
-    for section in sections:
-        ret.append({'sectionId': section.id, 'score': section.score})
-    return JsonResponse(ret)
-
-
-@login_required
-def analyzeScore(request):
-    student = request.user.student
-    scores = student.score.all()
-    ret = [avg_score, GP, GPA, Rank, Credit_earned, Credit_required]
-    return JsonResponse(ret)
+# @login_required
+# def inputScore(request):
+#     if request.user.instructor is None:
+#         return HttpResponse('You are not a instrctor!')
+#     section = section.Objects.get(id=request.POST['sectionId'])
+#     students = section.student_set.all()
+#     for student in students:
+#         insert
+#         record
+#         to
+#         Takes
+#     return HttpResponse('success')
+#
+#
+# @login_required
+# def teacherGetScore(request):
+#     if request.user.instructor is None:
+#         return HttpResponse('You are not a instrctor!')
+#     section = section.Objects.get(id=request.POST['sectionId'])
+#     students = section.student_set.all()
+#     ret = []
+#     for student in students:
+#         ret.append({'id': student.id, 'score': student.score})
+#     return JsonResponse(ret)
+#
+#
+# @login_required
+# def changeScore(request):
+#     if request.user.instructor is None:
+#         return HttpResponse('You are not a instrctor!')
+#     if request.user.check_password(request.POST['password']) is False:
+#         return HttpResponse('Password is wrong!')
+#     section = section.Objects.get(id=request.POST['sectionId'])
+#     student = request.POST['studentId']
+#     student.score = request.POST['score']
+#     return HttpResponse('success')
+#
+#
+# @login_required
+# def studentGetScore(request):
+#     if request.user.student is None:
+#         return HttpResponse('You are not a student!')
+#     student = request.user.student
+#     sections = student.section_set.all()
+#     for section in sections:
+#         ret.append({'sectionId': section.id, 'score': section.score})
+#     return JsonResponse(ret)
+#
+#
+# @login_required
+# def analyzeScore(request):
+#     student = request.user.student
+#     scores = student.score.all()
+#     ret = [avg_score, GP, GPA, Rank, Credit_earned, Credit_required]
+#     return JsonResponse(ret)
