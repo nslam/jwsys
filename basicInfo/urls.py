6453e92a62846db437847bdd9b78c604b2cb325d
TheLam <nslam@zju.edu.cn>
2017-06-07 18:50:46 +0800

    none

--- basicInfo/urls.py
+++ basicInfo/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import url
 
 from . import views
-urlpatterns={
+urlpatterns=[
     url(r'^$', views.index, name='index'),
     url(r'^login/$', views.login),
     url(r'^setPassword/$', views.setPassword),
     url(r'^changeInfo/$', views.changeInfo),
     url(r'^addStudent/$', views.addStudent),
     url(r'^addInstructor/$', views.addInstructor),
-}
+]
