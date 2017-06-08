938d28c214220c5a5cbcbff4c9da06966cb539e3
keys961 <keys961@hotmail.com>
2017-06-08 08:29:29 +0800

    Implement login & changepwd

--- basicInfo/views.py
+++ basicInfo/views.py
@@ -3,7 +3,7 @@ from .models import *
 from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
 from django.contrib import auth
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 
 
@@ -16,7 +16,6 @@ def login(request):
     username = request.POST['username']
     password = request.POST['password']
     type = request.POST['type']
-    status = 'true'
     # logs = Log.objects.filter(event__contains=request.POST['username'])
     # if login too frequent:
     #     return HttpResponse('Log in too frequent!')
@@ -28,14 +27,11 @@ def login(request):
     if user is None:
         status = 'Password is not correct!'
         return render(request, 'login.html', {'status': status})
-    # Need to test the type of the user according to DB
-    # Session or Cookies may be needed, request.user needs to change globally
-    # m = Member.objects.get(username=username)
-    # request.session['member_id'] = m.id
     if not user.is_active:
         status = 'User is not active!'
         return render(request, 'login.html', {'status': status})
     auth.login(request=request, user=user)
+    # Check type !!!!! Here I do not check that....
     if type == 'Student':
         return render(request, 'student_main.html', {'username': username})
     elif type == 'Teacher':
@@ -49,18 +45,17 @@ def setPassword(request):
     if request.method == 'GET':
         return render(request, 'changepwd.html', {'status':'true'})
     else:
-
         originPwd = request.POST['originPwd']
         newPwd = request.POST['newPwd']
         newPwdAgain = request.POST['newPwdAgain']
-        #Check success, modify failed
         flag = request.user.check_password(originPwd)
         if flag is False:
             status = 'Original password is not correct!'
         elif newPwd != newPwdAgain:
             status = 'New passwords are not same!'
         else:
-            request.user.set_password(request.POST['newPwd'])
+            request.user.set_password(newPwd)
+            request.user.save()
             status = 'Password changed successful!'
         return render(request, 'changepwd.html', {'status':status})
 
@@ -169,58 +164,58 @@ def searchLog(request):
     return JsonResponse(ret)
 
 
-# @login_required
-# def inputScore(request):
-#     if request.user.instructor is None:
-#         return HttpResponse('You are not a instrctor!')
-#     section = section.Objects.get(id=request.POST['sectionId'])
-#     students = section.student_set.all()
-#     for student in students:
-#         insert
-#         record
-#         to
-#         Takes
-#     return HttpResponse('success')
-#
-#
-# @login_required
-# def teacherGetScore(request):
-#     if request.user.instructor is None:
-#         return HttpResponse('You are not a instrctor!')
-#     section = section.Objects.get(id=request.POST['sectionId'])
-#     students = section.student_set.all()
-#     ret = []
-#     for student in students:
-#         ret.append({'id': student.id, 'score': student.score})
-#     return JsonResponse(ret)
-#
-#
-# @login_required
-# def changeScore(request):
-#     if request.user.instructor is None:
-#         return HttpResponse('You are not a instrctor!')
-#     if request.user.check_password(request.POST['password']) is False:
-#         return HttpResponse('Password is wrong!')
-#     section = section.Objects.get(id=request.POST['sectionId'])
-#     student = request.POST['studentId']
-#     student.score = request.POST['score']
-#     return HttpResponse('success')
-#
-#
-# @login_required
-# def studentGetScore(request):
-#     if request.user.student is None:
-#         return HttpResponse('You are not a student!')
-#     student = request.user.student
-#     sections = student.section_set.all()
-#     for section in sections:
-#         ret.append({'sectionId': section.id, 'score': section.score})
-#     return JsonResponse(ret)
-#
-#
-# @login_required
-# def analyzeScore(request):
-#     student = request.user.student
-#     scores = student.score.all()
-#     ret = [avg_score, GP, GPA, Rank, Credit_earned, Credit_required]
-#     return JsonResponse(ret)
+@login_required
+def inputScore(request):
+    if request.user.instructor is None:
+        return HttpResponse('You are not a instrctor!')
+    section = section.Objects.get(id=request.POST['sectionId'])
+    students = section.student_set.all()
+    for student in students:
+        insert
+        record
+        to
+        Takes
+    return HttpResponse('success')
+
+
+@login_required
+def teacherGetScore(request):
+    if request.user.instructor is None:
+        return HttpResponse('You are not a instrctor!')
+    section = section.Objects.get(id=request.POST['sectionId'])
+    students = section.student_set.all()
+    ret = []
+    for student in students:
+        ret.append({'id': student.id, 'score': student.score})
+    return JsonResponse(ret)
+
+
+@login_required
+def changeScore(request):
+    if request.user.instructor is None:
+        return HttpResponse('You are not a instrctor!')
+    if request.user.check_password(request.POST['password']) is False:
+        return HttpResponse('Password is wrong!')
+    section = section.Objects.get(id=request.POST['sectionId'])
+    student = request.POST['studentId']
+    student.score = request.POST['score']
+    return HttpResponse('success')
+
+
+@login_required
+def studentGetScore(request):
+    if request.user.student is None:
+        return HttpResponse('You are not a student!')
+    student = request.user.student
+    sections = student.section_set.all()
+    for section in sections:
+        ret.append({'sectionId': section.id, 'score': section.score})
+    return JsonResponse(ret)
+
+
+@login_required
+def analyzeScore(request):
+    student = request.user.student
+    scores = student.score.all()
+    ret = [avg_score, GP, GPA, Rank, Credit_earned, Credit_required]
+    return JsonResponse(ret)
