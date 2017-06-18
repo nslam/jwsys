b6a1e89807560e3a5b108f6f4a71d1f22fe11948
keys961 <keys961@hotmail.com>
2017-06-18 23:37:32 +0800

    1. 老师添加课程已经可用 2. 查看课程没有显示原始模板字段，即正常 3. 学生成绩查询以及老师成绩录入查询未检查 4. 管理员修改课程是不能更改课程号的 5. 用户查询更改删除以及课程的删除更改是可用的，请检查自己的数据库

--- jwsys/settings.py
+++ jwsys/settings.py
@@ -77,7 +77,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'jwsysDB',
         'USER': 'root',
-        'PASSWORD': '445416824',
+        'PASSWORD': '123456',
         'HOST': 'localhost',
         'PORT': '3306',
     }
