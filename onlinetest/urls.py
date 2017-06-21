cd3edfbe7a42f9bfae138b1f409e7a548f6004c4
tomatolike <695268122@qq.com>
2017-06-22 05:38:42 +0800

    Add files via upload

---
+++ onlinetest/urls.py
@@ -0,0 +1,26 @@
+from django.conf.urls import url
+from . import views
+urlpatterns = [
+    
+    url(r'^teacher1/', views.teacher1),
+    url(r'^teacher3/', views.teacher3),
+    url(r'^student1/', views.student1),
+    url(r'^student2/', views.student2),
+    url(r'^dotest/(\w+)/$', views.dotest),
+    url(r'^paper1/',views.paper1),
+    url(r'^paper2/(\w+)/$', views.paper2),
+    url(r'^teacher1delete/(\w+)/$', views.deletepaper),
+    url(r'^buildnewpaper/', views.newpaper),
+    url(r'^teacher3newpd/', views.newpdquestion),
+    url(r'^teacher3newxz/', views.newxzquestion),
+	url(r'^teacher3delete/(\w+)/$', views.deletequestion),
+	url(r'^teacher3change/(\w+)/$', views.changequestion),
+	url(r'^paperchinfo/(\w+)/$', views.paperchinfo),
+	url(r'paper2add/p1(\w+)p2(\w+)/$', views.paperadd),
+	url(r'paper2delete/p1(\w+)p2(\w+)/$', views.paperdelete),
+    url(r'teacher3search/', views.searchquestion),
+    url(r'teacher2/', views.statistics),
+
+
+]
+
